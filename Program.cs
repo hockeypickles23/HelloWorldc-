62fe1ef0324df122d00c09d7a4b3cb485e4d48df
BuildTools <unconfigured@null.spigotmc.org>
2018-01-15 12:04:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1               //namespace given by file name
+{
+    class Program                   //class name under the main file
+    {
+        static void Main(string[] args)         //main exe 
+        {
+            Console.WriteLine("Hello World");       //hello world statement given by write line
+        }
+    }
+}
